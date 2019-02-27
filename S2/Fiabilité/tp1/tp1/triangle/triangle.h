274b55f5e107156349c84f4d8b4eff55a91949aa
DESKTOP-RPDFU4Q\gonzo <gonzo222@hotmail.fr>
2019-02-27 15:53:43 +0100

    suppréssion

--- S2/Fiabilité/tp1/tp1/triangle/triangle.h
+++
@@ -1,9 +0,0 @@
-/**
-* @author Sébastien Gonzalez
-* @author Younes Rouahbia
-**/
-#include <stdlib.h>
-#include <stdio.h>
-
-double* readData(char* nomFichier);
-int typeTriangle(double a, double b, double c);
